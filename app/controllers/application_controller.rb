2d1808776d0e95b7a15885f10c65dba09d3a210e
NguyenThu2808 <nguyenthu280896@gmail.com>
2018-08-27 13:59:27 +0700

    Rating book (star)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+  include ReviewsHelper
+
+  def logged_in_user
+    return if logged_in?
+    store_location
+    flash[:danger] = t ".login_request"
+    redirect_to login_url
+  end
 end
